2e1f6d7803f83d3bc9f7a4c0a481530590ecbffa
pprp <1115957667@qq.com>
2020-01-05 12:03:23 +0800

    Update non_local_embedded_gaussian.py

--- attention/Non-local/Non-Local_pytorch_0.4.1_to_1.1.0/lib/non_local_embedded_gaussian.py
+++ attention/Non-local/Non-Local_pytorch_0.4.1_to_1.1.0/lib/non_local_embedded_gaussian.py
@@ -12,7 +12,12 @@ class _NonLocalBlockND(nn.Module):
             bn_layer=bn_layer)
 
     """
-    def __init__(self, in_channels, inter_channels=None, dimension=3, sub_sample=True, bn_layer=True):
+    def __init__(self,
+                 in_channels,
+                 inter_channels=None,
+                 dimension=3,
+                 sub_sample=True,
+                 bn_layer=True):
         super(_NonLocalBlockND, self).__init__()
 
         assert dimension in [1, 2, 3]
@@ -42,27 +47,40 @@ class _NonLocalBlockND(nn.Module):
             max_pool_layer = nn.MaxPool1d(kernel_size=(2))
             bn = nn.BatchNorm1d
 
-        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
-                         kernel_size=1, stride=1, padding=0)
+        self.g = conv_nd(in_channels=self.in_channels,
+                         out_channels=self.inter_channels,
+                         kernel_size=1,
+                         stride=1,
+                         padding=0)
 
         if bn_layer:
             self.W = nn.Sequential(
-                conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
-                        kernel_size=1, stride=1, padding=0),
-                bn(self.in_channels)
-            )
+                conv_nd(in_channels=self.inter_channels,
+                        out_channels=self.in_channels,
+                        kernel_size=1,
+                        stride=1,
+                        padding=0), bn(self.in_channels))
             nn.init.constant_(self.W[1].weight, 0)
             nn.init.constant_(self.W[1].bias, 0)
         else:
-            self.W = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
-                             kernel_size=1, stride=1, padding=0)
+            self.W = conv_nd(in_channels=self.inter_channels,
+                             out_channels=self.in_channels,
+                             kernel_size=1,
+                             stride=1,
+                             padding=0)
             nn.init.constant_(self.W.weight, 0)
             nn.init.constant_(self.W.bias, 0)
 
-        self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
-                             kernel_size=1, stride=1, padding=0)
-        self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
-                           kernel_size=1, stride=1, padding=0)
+        self.theta = conv_nd(in_channels=self.in_channels,
+                             out_channels=self.inter_channels,
+                             kernel_size=1,
+                             stride=1,
+                             padding=0)
+        self.phi = conv_nd(in_channels=self.in_channels,
+                           out_channels=self.inter_channels,
+                           kernel_size=1,
+                           stride=1,
+                           padding=0)
 
         if sub_sample:
             self.g = nn.Sequential(self.g, max_pool_layer)
@@ -81,7 +99,9 @@ class _NonLocalBlockND(nn.Module):
 
         theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
         theta_x = theta_x.permute(0, 2, 1)
+
         phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
+        
         f = torch.matmul(theta_x, phi_x)
         f_div_C = F.softmax(f, dim=-1)
 
@@ -95,33 +115,49 @@ class _NonLocalBlockND(nn.Module):
 
 
 class NONLocalBlock1D(_NonLocalBlockND):
-    def __init__(self, in_channels, inter_channels=None, sub_sample=True, bn_layer=True):
+    def __init__(self,
+                 in_channels,
+                 inter_channels=None,
+                 sub_sample=True,
+                 bn_layer=True):
         super(NONLocalBlock1D, self).__init__(in_channels,
                                               inter_channels=inter_channels,
-                                              dimension=1, sub_sample=sub_sample,
+                                              dimension=1,
+                                              sub_sample=sub_sample,
                                               bn_layer=bn_layer)
 
 
 class NONLocalBlock2D(_NonLocalBlockND):
-    def __init__(self, in_channels, inter_channels=None, sub_sample=True, bn_layer=True):
+    def __init__(self,
+                 in_channels,
+                 inter_channels=None,
+                 sub_sample=True,
+                 bn_layer=True):
         super(NONLocalBlock2D, self).__init__(in_channels,
                                               inter_channels=inter_channels,
-                                              dimension=2, sub_sample=sub_sample,
+                                              dimension=2,
+                                              sub_sample=sub_sample,
                                               bn_layer=bn_layer)
 
 
 class NONLocalBlock3D(_NonLocalBlockND):
-    def __init__(self, in_channels, inter_channels=None, sub_sample=True, bn_layer=True):
+    def __init__(self,
+                 in_channels,
+                 inter_channels=None,
+                 sub_sample=True,
+                 bn_layer=True):
         super(NONLocalBlock3D, self).__init__(in_channels,
                                               inter_channels=inter_channels,
-                                              dimension=3, sub_sample=sub_sample,
+                                              dimension=3,
+                                              sub_sample=sub_sample,
                                               bn_layer=bn_layer)
 
 
 if __name__ == '__main__':
     import torch
 
-    for (sub_sample, bn_layer) in [(True, True), (False, False), (True, False), (False, True)]:
+    for (sub_sample, bn_layer) in [(True, True), (False, False), (True, False),
+                                   (False, True)]:
         img = torch.zeros(2, 3, 20)
         net = NONLocalBlock1D(3, sub_sample=sub_sample, bn_layer=bn_layer)
         out = net(img)
