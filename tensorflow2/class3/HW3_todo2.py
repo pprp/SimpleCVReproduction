6c1192d54a16ce5496369acca2b96b1f8c612ed6
pprp <1115957667@qq.com>
2020-03-14 10:19:20 +0800

    Update HW3_todo2.py

--- tensorflow2/class3/HW3_todo2.py
+++ tensorflow2/class3/HW3_todo2.py
@@ -15,16 +15,15 @@ train_ds = tf.data.Dataset.from_tensor_slices(
     (x_train, y_train)).shuffle(10000).batch(32)
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
+
 class MyModel(Model):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.conv1 = Conv2D(32, 3, activation='relu')
         self.flatten = Flatten()
-        self.d1 = Dense(128, activation='relu')
+        self.d1 = Dense(500, activation='relu')
         self.d2 = Dense(10, activation='softmax')
 
     def call(self, x):
-        x = self.conv1(x)
         x = self.flatten(x)
         x = self.d1(x)
         return self.d2(x)
@@ -81,8 +80,7 @@ for epoch in range(EPOCHS):
         test_step(test_images, test_labels)
 
     template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
-    print(template.format(epoch+1,
-                          train_loss.result(),
-                          train_accuracy.result()*100,
-                          test_loss.result(),
-                          test_accuracy.result()*100))
+    print(
+        template.format(epoch + 1, train_loss.result(),
+                        train_accuracy.result() * 100, test_loss.result(),
+                        test_accuracy.result() * 100))
