fc1dbb211901f79fcc9cb6c75b1e736aa90644b6
pprp <1115957667@qq.com>
2020-03-15 23:36:35 +0800

    update cbam

--- attention/CBAM/cbam.py
+++ attention/CBAM/cbam.py
@@ -1,17 +1,26 @@
 import torch
-
 import torch.nn as nn
 
 
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes,
+                     out_planes,
+                     kernel_size=3,
+                     stride=stride,
+                     padding=1,
+                     bias=False)
+
+
 class ChannelAttention(nn.Module):
-    def __init__(self, in_planes, rotio=16):
+    def __init__(self, in_planes, ratio=4):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
 
         self.sharedMLP = nn.Sequential(
             nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False), nn.ReLU(),
-            nn.Conv2d(in_planes // rotio, in_planes, 1, bias=False))
+            nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False))
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -19,13 +28,14 @@ class ChannelAttention(nn.Module):
         maxout = self.sharedMLP(self.max_pool(x))
         return self.sigmoid(avgout + maxout)
 
+
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
-        assert kernel_size in (3,7), "kernel size must be 3 or 7"
+        assert kernel_size in (3, 7), "kernel size must be 3 or 7"
         padding = 3 if kernel_size == 7 else 1
 
-        self.conv = nn.Conv2d(2,1,kernel_size, padding=padding, bias=False)
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -67,9 +77,24 @@ class BasicBlock(nn.Module):
         out = self.sa(out) * out  # 广播机制
 
         if self.downsample is not None:
+            print("downsampling")
             residual = self.downsample(x)
+
+        print(out.shape, residual.shape)
 
         out += residual
         out = self.relu(out)
 
         return out
+
+
+if __name__ == "__main__":
+    downsample = nn.Sequential(
+        nn.Conv2d(16, 32, kernel_size=1, stride=1, bias=False),
+        nn.BatchNorm2d(32))
+
+    x = torch.ones(3, 16, 32, 32)
+
+    model = BasicBlock(16, 32, stride=1, downsample=downsample)
+
+    print(model(x).shape)
