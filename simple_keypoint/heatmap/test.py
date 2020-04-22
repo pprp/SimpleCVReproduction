2f7a444745bba6d19460d78e6b77fa343114bf7c
peijieDong <1115957667@qq.com>
2020-04-22 22:09:47 +0800

    Update test.py

--- simple_keypoint/heatmap/test.py
+++ simple_keypoint/heatmap/test.py
@@ -1,5 +1,10 @@
+import argparse
+
+import numpy as np
 import torch
 import torch.nn.functional as F
+import torchvision
+from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
@@ -7,6 +12,43 @@ from datasets import KeyPointDatasets
 from model import KeyPointModel
 from utils import *
 from utils import compute_loss
+
+parser = argparse.ArgumentParser(description="model path")
+parser.add_argument('--model', type=str, default="")
+
+args = parser.parse_args()
+
+
+def _gather_feature(feat, ind, mask=None):
+    dim = feat.size(2)
+    ind = ind.unsqueeze(2).expand(ind.size(0), ind.size(1), dim)
+    feat = feat.gather(1, ind)
+    if mask is not None:
+        mask = mask.unsqueeze(2).expand_as(feat)
+        feat = feat[mask]
+        feat = feat.view(-1, dim)
+    return feat
+
+
+def _topk(scores, K=40):
+    batch, cat, height, width = scores.size()
+
+    topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
+
+    topk_inds = topk_inds % (height * width)
+    topk_ys = (topk_inds / width).int().float()
+    topk_xs = (topk_inds % width).int().float()
+
+    topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
+    topk_clses = (topk_ind / K).int()
+    topk_inds = _gather_feature(topk_inds.view(
+        batch, -1, 1), topk_ind).view(batch, K)
+    topk_ys = _gather_feature(topk_ys.view(
+        batch, -1, 1), topk_ind).view(batch, K)
+    topk_xs = _gather_feature(topk_xs.view(
+        batch, -1, 1), topk_ind).view(batch, K)
+
+    return topk_score, topk_inds, topk_clses, topk_ys, topk_xs
 
 
 def _nms(heat, kernel=3):
@@ -41,13 +83,18 @@ if __name__ == "__main__":
                             collate_fn=dataset.collect_fn)
 
     model = KeyPointModel()
+    model.load_state_dict(torch.load(args.model))
 
     for iter, (image, label) in enumerate(dataloader):
-        print(image.shape)
+        # print(image.shape)
         bs = image.shape[0]
         hm = model(image)
 
         hm = _nms(hm)
+
+        scores, inds, clses, ys, xs = _topk(hm,K=1)
+
+        print(scores, '\n', inds, '\n', clses, '\n', ys, '\n', xs)
 
         hm = hm.detach().numpy()
 
@@ -57,9 +104,10 @@ if __name__ == "__main__":
             hm = hm/np.max(hm)
             hm = normalization(hm)
             hm = np.uint8(255 * hm)
+            hm = hm[0]
             # heatmap = torch.sigmoid(heatmap)
 
-            hm = cv2.cvtColor(hm,cv2.COLOR_RGB2BGR)
+            # hm = cv2.cvtColor(hm, cv2.COLOR_RGB2BGR)
 
             hm = cv2.applyColorMap(hm, cv2.COLORMAP_JET)
 
