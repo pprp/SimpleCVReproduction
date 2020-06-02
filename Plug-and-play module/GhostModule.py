6c09d001a9a7d4b7221d2990e863baea45d24503
peijieDong <1115957667@qq.com>
2020-06-02 16:02:30 +0800

    delete some from ghostnet :smile:

--- Plug-and-play module/GhostModule.py
+++ Plug-and-play module/GhostModule.py
@@ -2,33 +2,6 @@ import torch.nn as nn
 import math
 import torch
 
-
-class SELayer(nn.Module):
-    def __init__(self, channel, reduction=4):
-        super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel),)
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        y = torch.clamp(y, 0, 1)
-        return x * y
-
-
-def depthwise_conv(inp, oup, kernel_size=3, stride=1, relu=False):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, kernel_size, stride,
-                  kernel_size//2, groups=inp, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.ReLU(inplace=True) if relu else nn.Sequential(),
-    )
-
-
 class GhostModule(nn.Module):
     def __init__(self, inp, oup, kernel_size=1, ratio=2, dw_size=3, stride=1, relu=True):
         super(GhostModule, self).__init__()
