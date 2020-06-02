75d150b312d6d55885ca26f7361ccafeef2a25c8
peijieDong <1115957667@qq.com>
2020-06-02 15:55:42 +0800

    update GhostModule

---
+++ Plug-and-play module/GhostModule.py
@@ -0,0 +1,67 @@
+import torch.nn as nn
+import math
+import torch
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=4):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel),)
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        y = torch.clamp(y, 0, 1)
+        return x * y
+
+
+def depthwise_conv(inp, oup, kernel_size=3, stride=1, relu=False):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, kernel_size, stride,
+                  kernel_size//2, groups=inp, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU(inplace=True) if relu else nn.Sequential(),
+    )
+
+
+class GhostModule(nn.Module):
+    def __init__(self, inp, oup, kernel_size=1, ratio=2, dw_size=3, stride=1, relu=True):
+        super(GhostModule, self).__init__()
+        self.oup = oup
+        init_channels = math.ceil(oup / ratio)
+        new_channels = init_channels*(ratio-1)
+
+        self.primary_conv = nn.Sequential(
+            nn.Conv2d(inp, init_channels, kernel_size,
+                      stride, kernel_size//2, bias=False),
+            nn.BatchNorm2d(init_channels),
+            nn.ReLU(inplace=True) if relu else nn.Sequential(),
+        )
+
+        self.cheap_operation = nn.Sequential(
+            nn.Conv2d(init_channels, new_channels, dw_size, 1,
+                      dw_size//2, groups=init_channels, bias=False),
+            nn.BatchNorm2d(new_channels),
+            nn.ReLU(inplace=True) if relu else nn.Sequential(),
+        )
+
+    def forward(self, x):
+        x1 = self.primary_conv(x)
+        x2 = self.cheap_operation(x1)
+        out = torch.cat([x1, x2], dim=1)
+        return out[:, :self.oup, :, :]
+
+
+if __name__ == "__main__":
+    model = GhostModule(128, 256, 3)
+
+    in_tensor = torch.zeros((2, 128, 64, 64))
+
+    out_tensor = model(in_tensor)
+
+    print(out_tensor.shape)
