adc1e51391278487e2751914a894a91a5ce776fb
PJDong <1115957667@qq.com>
2021-03-14 15:34:28 +0800

    update cifar trick

---
+++ cifarTrick/torch_post_train/cxh.py
@@ -0,0 +1,452 @@
+
+
+'''Train CIFAR10 with PyTorch.'''
+from __future__ import print_function
+# from torch.utils.tensorboard import SummaryWriter
+from torch.autograd import Variable
+from cutout import Cutout
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import sys
+import argparse
+import logging
+import time
+# from models.vgg import CNN
+from utils import progress_bar
+from dropblock import DropBlock2D, LinearScheduler
+from torch.nn import Module
+import nni
+
+# torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+# from torchviz import make_dot
+
+import os
+# os.environ["PATH"] += os.pathsep + 'D:/Graphviz2.38/bin'
+
+
+class SiLU(Module):
+    """SiLU activation function (also known as Swish): x * sigmoid(x)."""
+
+    # Note: will be part of Pytorch 1.7, at which point can remove this.
+
+    def __init__(self):
+        super(SiLU, self).__init__()
+
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+def active_fn(cfg):
+    activation_fun = cfg.active_function
+    if activation_fun == "relu":
+        return nn.ReLU(inplace=True)
+    elif activation_fun == "silu":
+        return SiLU()
+
+
+class SqueezeAndExcitation(nn.Module):
+    """Squeeze-and-Excitation module.
+
+    See: https://arxiv.org/abs/1709.01507
+    """
+
+    def __init__(self,
+                 n_feature,
+                 n_hidden,
+                 spatial_dims=[2, 3],
+                 activation_fun=None,
+                 ):
+        super(SqueezeAndExcitation, self).__init__()
+        self.n_feature = n_feature
+        self.n_hidden = n_hidden
+        self.spatial_dims = spatial_dims
+        self.se_reduce = nn.Conv2d(n_feature, n_hidden, 1, bias=True)
+        self.se_expand = nn.Conv2d(n_hidden, n_feature, 1, bias=True)
+        if activation_fun == "relu":
+            self.active_fn = nn.ReLU(inplace=True)
+        elif activation_fun == "silu":
+            self.active_fn = SiLU()
+
+    def forward(self, x):
+        se_tensor = x.mean(self.spatial_dims, keepdim=True)
+        se_tensor = self.se_expand(self.active_fn(self.se_reduce(se_tensor)))
+        return torch.sigmoid(se_tensor) * x
+#
+
+
+class CXH_plain(Module):
+    def __init__(self):
+        super(CXH_plain, self).__init__()
+        self.stem = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(3)
+        )
+        self.a1 = nn.Sequential(
+            nn.BatchNorm2d(3),
+            nn.Conv2d(3, 48, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(48, 48, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(48, 512, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(256)
+        )
+        self.a2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True)
+        )
+        self.b = nn.Sequential(
+            nn.Conv2d(259, 256, kernel_size=1, stride=1, padding=0),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=5, stride=1, padding=2),
+
+        )
+        self.c1 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(1024),
+            nn.Conv2d(1024, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.c2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(1024, 64, kernel_size=1, stride=1, padding=0),
+        )
+        self.d = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Dropout(p=0.25)
+        )
+        self.f1 = nn.Linear(64, 64)
+        self.active = nn.ReLU(inplace=True)
+        self.f2 = nn.Linear(64, 10)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        stem = self.stem(x)
+        a1 = self.a1(stem)
+        a2 = self.a2(stem)
+        bf = torch.cat((a1, a2), 1)
+        b = self.b(bf)
+        c1 = self.c1(b)
+        c2 = self.c2(b)
+        df = c1+c2
+        d = self.d(df)
+        d = torch.flatten(d, 1)
+        d = self.f1(d)
+        d = self.active(d)
+        d = self.f2(d)
+        return d
+
+
+class CXH_SE(Module):
+    def __init__(self):
+        super(CXH_SE, self).__init__()
+        self.stem = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(3)
+        )
+        self.a1 = nn.Sequential(
+            nn.BatchNorm2d(3),
+            nn.Conv2d(3, 48, kernel_size=3, stride=1, padding=1),
+            SqueezeAndExcitation(48, 3, activation_fun='relu'),
+            nn.Conv2d(48, 48, kernel_size=1, stride=1, padding=0),
+            SqueezeAndExcitation(48, 3, activation_fun='relu'),
+            nn.Conv2d(48, 512, kernel_size=3, stride=1, padding=1),
+            SqueezeAndExcitation(512, 32, activation_fun='relu'),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
+            SqueezeAndExcitation(256, 16, activation_fun='relu'),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            SqueezeAndExcitation(128, 8, activation_fun='relu'),
+            nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2),
+            SqueezeAndExcitation(256, 16, activation_fun='relu'),
+            nn.BatchNorm2d(256)
+        )
+        self.a2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True)
+        )
+        self.b = nn.Sequential(
+            nn.Conv2d(259, 256, kernel_size=1, stride=1, padding=0),
+            SqueezeAndExcitation(256, 16, activation_fun='relu'),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            SqueezeAndExcitation(512, 32, activation_fun='relu'),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 1024, kernel_size=5, stride=1, padding=2),
+            SqueezeAndExcitation(1024, 64, activation_fun='relu'),
+        )
+        self.c1 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(1024),
+            nn.Conv2d(1024, 64, kernel_size=3, stride=1, padding=1),
+            SqueezeAndExcitation(64, 4, activation_fun='relu'),
+            nn.ReLU(inplace=True)
+        )
+        self.c2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(1024, 64, kernel_size=1, stride=1, padding=0),
+            SqueezeAndExcitation(64, 4, activation_fun='relu'),
+        )
+        self.d = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Dropout(p=0.25)
+        )
+        self.f1 = nn.Linear(64, 64)
+        self.active = nn.ReLU(inplace=True)
+        self.f2 = nn.Linear(64, 10)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        stem = self.stem(x)
+        a1 = self.a1(stem)
+        a2 = self.a2(stem)
+        bf = torch.cat((a1, a2), 1)
+        b = self.b(bf)
+        c1 = self.c1(b)
+        c2 = self.c2(b)
+        df = c1+c2
+        d = self.d(df)
+        d = torch.flatten(d, 1)
+        d = self.f1(d)
+        d = self.active(d)
+        d = self.f2(d)
+        return d
+
+
+class CXH(Module):
+    def __init__(self):
+        super(CXH, self).__init__()
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.a1 = nn.Sequential(
+            nn.Conv2d(64, 256, kernel_size=1, stride=2, padding=0),
+            nn.BatchNorm2d(256),
+        )
+        self.a2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+        )
+        self.b = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.c1 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256)
+        )
+        self.d = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Conv2d(256, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Dropout(p=0.25)
+        )
+        self.f1 = nn.Linear(64, 64)
+        self.active = nn.ReLU(inplace=True)
+        self.f2 = nn.Linear(64, 10)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        stem = self.stem(x)
+        a1 = self.a1(stem)
+        a2 = self.a2(stem)
+        bf = a1+a2
+        b = self.b(bf)
+        c1 = self.c1(b)
+        df = c1+b
+        d = self.d(df)
+        d = torch.flatten(d, 1)
+        d = self.f1(d)
+        d = self.active(d)
+        d = self.f2(d)
+        return d
+
+
+class CXH_Squeeze_Excitation(Module):
+    def __init__(self):
+        super(CXH_Squeeze_Excitation, self).__init__()
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            # se
+            SqueezeAndExcitation(64, 16, activation_fun='relu'),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            # se
+            SqueezeAndExcitation(64, 16, activation_fun='relu')
+        )
+        self.a1 = nn.Sequential(
+            nn.Conv2d(64, 256, kernel_size=1, stride=2, padding=0),
+            nn.BatchNorm2d(256),
+            SqueezeAndExcitation(256, 64, activation_fun='relu')
+        )
+        self.a2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            SqueezeAndExcitation(256, 64, activation_fun='relu')
+        )
+        self.b = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            SqueezeAndExcitation(256, 64, activation_fun='relu')
+        )
+        self.c1 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+
+            SqueezeAndExcitation(256, 64, activation_fun='relu')
+        )
+        self.d = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            SqueezeAndExcitation(256, 64, activation_fun='relu'),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            SqueezeAndExcitation(256, 64, activation_fun='relu'),
+
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Conv2d(256, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            SqueezeAndExcitation(64, 16, activation_fun='relu'),
+
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Dropout(p=0.25)
+        )
+        self.f1 = nn.Linear(64, 64)
+        self.active = nn.ReLU(inplace=True)
+        self.f2 = nn.Linear(64, 10)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        stem = self.stem(x)
+        a1 = self.a1(stem)
+        a2 = self.a2(stem)
+        bf = a1+a2
+        b = self.b(bf)
+        c1 = self.c1(b)
+        df = c1+b
+        d = self.d(df)
+        d = torch.flatten(d, 1)
+        d = self.f1(d)
+        d = self.active(d)
+        d = self.f2(d)
+        return d
+
+
+if __name__ == '__main__':
+    net = CXH()
+    x = torch.randn(2, 3, 32, 32)
+    y = net(x)
+    print(y.size())
+    # g = make_dot(y)
+    # g.render('EENA', view=True)
---
+++ cifarTrick/torch_post_train/train_CXH.py
@@ -0,0 +1,210 @@
+'''Train CIFAR10 with PyTorch.'''
+from __future__ import print_function
+# from torch.utils.tensorboard import SummaryWriter
+from torch.autograd import Variable
+from cutout import Cutout
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+from autoaugmentation import CIFAR10Policy
+
+import os
+import sys
+import argparse
+import logging
+import time
+from cxh import *
+from utils import progress_bar
+
+
+import nni
+
+# sys.stdout = Logger()
+
+trainloader = None
+testloader = None
+net = None
+criterion = None
+optimizer = None
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0.0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+
+def mixup_data(x, y, alpha=1.0):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size).cuda()
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
+def prepare(args):
+    global trainloader
+    global testloader
+    global net
+    global criterion
+    global optimizer
+    global scheduler
+
+    # Data
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        CIFAR10Policy(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010))
+        # Cutout(n_holes=args.n_holes, length=args.length)
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=512, shuffle=True, num_workers=4)
+
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=512, shuffle=False, num_workers=4)
+
+    #classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # Model
+    print('==> Building model..')
+    net = CXH_Squeeze_Excitation() #CXH()
+
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.1,
+                          momentum=0.9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        optimizer, 1, 2)
+
+# Training
+
+
+def train(epoch, args):
+    global trainloader
+    global testloader
+    global net
+    global criterion
+    global optimizer
+    global scheduler
+
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    iters = len(trainloader)
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        # inputs, targets_a, targets_b, lam = mixup_data(inputs, targets,args.alpha)
+        # inputs, targets_a, targets_b = map(Variable, (inputs,targets_a, targets_b))
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        # loss = mixup_criterion(criterion, outputs, targets_a, targets_b, lam)
+        train_loss += loss.data
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+        # (lam * predicted.eq(targets_a.data).cpu().sum().float()
+        # + (1 - lam) * predicted.eq(targets_b.data).cpu().sum().float())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step(epoch + batch_idx / iters)
+
+        acc = 100.*correct/total
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+
+def test(epoch):
+    global best_acc
+    global trainloader
+    global testloader
+    global net
+    global criterion
+    global optimizer
+
+    # log_dir = os.path.join('tensorboard', 'test')
+    # test_writer = SummaryWriter(log_dir=log_dir)
+
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            acc = 100.*correct/total
+            # test_writer.add_scalar('Accuracy', acc, epoch*len(testloader) + batch_idx)
+            # test_writer.add_scalar('Loss', test_loss/(batch_idx+1), epoch*len(testloader) + batch_idx)
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        best_acc = acc
+    return acc, best_acc
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=600)
+    parser.add_argument('--alpha', default=1., type=float,
+                        help='mixup interpolation coefficient (default: 1)')
+    parser.add_argument('--n_holes', type=int, default=1,
+                        help='number of holes to cut out from image')
+    parser.add_argument('--length', type=int, default=16,
+                        help='length of the holes')
+    args, _ = parser.parse_known_args()
+    prepare(args)
+    acc = 0.0
+    best_acc = 0.0
+    for epoch in range(start_epoch, start_epoch+args.epochs):
+        train(epoch, args)
+        acc, best_acc = test(epoch)
+        nni.report_intermediate_result(acc)
+    print('best acc is:', best_acc)
+    nni.report_final_result(best_acc)
